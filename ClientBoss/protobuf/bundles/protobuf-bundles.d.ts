a155aabf760fdedd96124162d559388cb347667f
hongdawei <84318436@qq.com>
2018-07-03 16:32:36 +0800

    BUG修复

--- ClientBoss/protobuf/bundles/protobuf-bundles.d.ts
+++ ClientBoss/protobuf/bundles/protobuf-bundles.d.ts
@@ -9146,6 +9146,96 @@ declare namespace msg {
          */
         public toJSON(): { [k: string]: any };
     }
+
+    /** Properties of a GW2C_SendWechatInfo. */
+    interface IGW2C_SendWechatInfo {
+
+        /** GW2C_SendWechatInfo openid */
+        openid?: (string|null);
+    }
+
+    /** Represents a GW2C_SendWechatInfo. */
+    class GW2C_SendWechatInfo implements IGW2C_SendWechatInfo {
+
+        /**
+         * Constructs a new GW2C_SendWechatInfo.
+         * @param [properties] Properties to set
+         */
+        constructor(properties?: msg.IGW2C_SendWechatInfo);
+
+        /** GW2C_SendWechatInfo openid. */
+        public openid: string;
+
+        /**
+         * Creates a new GW2C_SendWechatInfo instance using the specified properties.
+         * @param [properties] Properties to set
+         * @returns GW2C_SendWechatInfo instance
+         */
+        public static create(properties?: msg.IGW2C_SendWechatInfo): msg.GW2C_SendWechatInfo;
+
+        /**
+         * Encodes the specified GW2C_SendWechatInfo message. Does not implicitly {@link msg.GW2C_SendWechatInfo.verify|verify} messages.
+         * @param message GW2C_SendWechatInfo message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encode(message: msg.IGW2C_SendWechatInfo, writer?: protobuf.Writer): protobuf.Writer;
+
+        /**
+         * Encodes the specified GW2C_SendWechatInfo message, length delimited. Does not implicitly {@link msg.GW2C_SendWechatInfo.verify|verify} messages.
+         * @param message GW2C_SendWechatInfo message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encodeDelimited(message: msg.IGW2C_SendWechatInfo, writer?: protobuf.Writer): protobuf.Writer;
+
+        /**
+         * Decodes a GW2C_SendWechatInfo message from the specified reader or buffer.
+         * @param reader Reader or buffer to decode from
+         * @param [length] Message length if known beforehand
+         * @returns GW2C_SendWechatInfo
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decode(reader: (protobuf.Reader|Uint8Array), length?: number): msg.GW2C_SendWechatInfo;
+
+        /**
+         * Decodes a GW2C_SendWechatInfo message from the specified reader or buffer, length delimited.
+         * @param reader Reader or buffer to decode from
+         * @returns GW2C_SendWechatInfo
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decodeDelimited(reader: (protobuf.Reader|Uint8Array)): msg.GW2C_SendWechatInfo;
+
+        /**
+         * Verifies a GW2C_SendWechatInfo message.
+         * @param message Plain object to verify
+         * @returns `null` if valid, otherwise the reason why it is not
+         */
+        public static verify(message: { [k: string]: any }): (string|null);
+
+        /**
+         * Creates a GW2C_SendWechatInfo message from a plain object. Also converts values to their respective internal types.
+         * @param object Plain object
+         * @returns GW2C_SendWechatInfo
+         */
+        public static fromObject(object: { [k: string]: any }): msg.GW2C_SendWechatInfo;
+
+        /**
+         * Creates a plain object from a GW2C_SendWechatInfo message. Also converts values to other types if specified.
+         * @param message GW2C_SendWechatInfo
+         * @param [options] Conversion options
+         * @returns Plain object
+         */
+        public static toObject(message: msg.GW2C_SendWechatInfo, options?: protobuf.IConversionOptions): { [k: string]: any };
+
+        /**
+         * Converts this GW2C_SendWechatInfo to JSON.
+         * @returns JSON object
+         */
+        public toJSON(): { [k: string]: any };
+    }
 }
 
 /** Namespace table. */
