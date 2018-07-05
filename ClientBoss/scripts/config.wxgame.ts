e17b26a4e6059220d8228f52a1fc8e23ac639f77
liulibo <490144445@qq.com>
2018-07-05 17:33:15 +0800

    微信分包

--- ClientBoss/scripts/config.wxgame.ts
+++ ClientBoss/scripts/config.wxgame.ts
@@ -2,7 +2,7 @@
 ///<reference path="api.d.ts"/>
 
 import * as path from 'path';
-import { UglifyPlugin, CompilePlugin, ManifestPlugin, ExmlPlugin, EmitResConfigFilePlugin, TextureMergerPlugin, CleanPlugin } from 'built-in';
+import { UglifyPlugin, CompilePlugin, ManifestPlugin, ExmlPlugin, EmitResConfigFilePlugin, TextureMergerPlugin, CleanPlugin, RenamePlugin, ResSplitPlugin } from 'built-in';
 import { WxgamePlugin } from './wxgame/wxgame';
 import { CustomPlugin } from './myplugin';
 import * as defaultConfig from './config';
@@ -12,8 +12,12 @@ const config: ResourceManagerConfig = {
     buildConfig: (params) => {
 
         const { target, command, projectName, version } = params;
-        const outputDir = `../${projectName}_wxgame`;
+
+        var outputDir: string = `../${projectName}_wxgame`;
+        var remote = `../${projectName}_wxremote`
+
         if (command == 'build') {
+
             return {
                 outputDir,
                 commands: [
@@ -21,6 +25,26 @@ const config: ResourceManagerConfig = {
                     new CompilePlugin({ libraryType: "debug", defines: { DEBUG: false, RELEASE: true } }),
                     new ExmlPlugin('commonjs'), // 非 EUI 项目关闭此设置
                     new WxgamePlugin(),
+
+                    new ResSplitPlugin({
+                        verbose: true, matchers: [
+                            { from: "resource/**/**.@(jpg|fnt|mp3)", to: `${remote}_alpha`},
+                            { from: "resource/assets/!(ball|login)/**", to: `${remote}_alpha`},
+                        ]
+                    }),
+
+                    new EmitResConfigFilePlugin({
+                        output: "resource/default.res.json",
+                        typeSelector: config.typeSelector,
+                        nameSelector: (p: string) => {
+                            var b = p.lastIndexOf(".");
+                            var t = "assets/"
+                            var name = p.substring(p.indexOf(t) + t.length, b);
+                            return name;
+                        },
+                        groupSelector: p => "preload"
+                    }),
+
                     new UglifyPlugin([{
                         sources: ["main.js"],
                         target: "main.min.js"
@@ -38,6 +62,32 @@ const config: ResourceManagerConfig = {
                     new CompilePlugin({ libraryType: "release", defines: { DEBUG: false, RELEASE: true } }),
                     new ExmlPlugin('commonjs'), // 非 EUI 项目关闭此设置
                     new WxgamePlugin(),
+
+                    new RenamePlugin({
+                        verbose: true, hash: "crc32", matchers: [
+                            { from: "resource/**/**", to: "[path][name]_[hash].[ext]" }
+                        ]
+                    }),
+
+                    new ResSplitPlugin({
+                        verbose: true, matchers: [
+                            { from: "resource/**/**.@(jpg|fnt|mp3)", to: remote },
+                            { from: "resource/assets/!(ball|login)/**", to: remote },
+                        ]
+                    }),
+
+                    new EmitResConfigFilePlugin({
+                        output: "resource/default.res.json",
+                        typeSelector: config.typeSelector,
+                        nameSelector: (p: string) => {
+                            var b = p.lastIndexOf(".");
+                            var t = "assets/"
+                            var name = p.substring(p.indexOf(t) + t.length, b);
+                            return name;
+                        },
+                        groupSelector: p => "preload"
+                    }),
+
                     new UglifyPlugin([{
                         sources: ["main.js"],
                         target: "main.min.js"
