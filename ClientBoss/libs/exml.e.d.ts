c794b8e00c7fcfdf7a676631de3911488cb655c5
hongdawei <84318436@qq.com>
2018-07-06 17:22:37 +0800

    增加充值弹框提示

--- ClientBoss/libs/exml.e.d.ts
+++ ClientBoss/libs/exml.e.d.ts
@@ -1,6 +1,10 @@
+declare class CommonDialogSkin extends eui.Skin{
+}
 declare class LoadingSkin extends eui.Skin{
 }
 declare class NetFailedSkin extends eui.Skin{
+}
+declare class AvatarSkin extends eui.Skin{
 }
 declare class BattleBagItemSkin extends eui.Skin{
 }
@@ -38,8 +42,6 @@ declare class BattleTimeBoomSkin extends eui.Skin{
 }
 declare class NewFile extends eui.Skin{
 }
-declare class AvatarSkin extends eui.Skin{
-}
 declare class CloseButtonSkin extends eui.Skin{
 }
 declare class IconButtonSkin extends eui.Skin{
@@ -54,13 +56,13 @@ declare class NormalLabelButtonSkin extends eui.Skin{
 }
 declare class ToggleButtonSkin extends eui.Skin{
 }
-declare class MainSceneSkin extends eui.Skin{
-}
-declare class UserPanelSkin extends eui.Skin{
-}
 declare class LoginSceneSkin extends eui.Skin{
 }
 declare class RegisterPanelSkin extends eui.Skin{
+}
+declare class MainSceneSkin extends eui.Skin{
+}
+declare class UserPanelSkin extends eui.Skin{
 }
 declare class RankItemSkin extends eui.Skin{
 }
